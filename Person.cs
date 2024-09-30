c04fc378aa0c7f1511743a461b029327d610939a
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-09-30 11:10:34 -0500

    Person Class Setup Complete

--- Person.cs
+++ Person.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 abstract class Person
 {
     // declare variables
@@ -30,5 +32,61 @@ abstract class Person
     public override string ToString()
     {
         return $"Email Address: {EmailAddress}, First Name: {FirstName}, Last Name: {LastName}, Phone Number {PhoneNumber}";
+    }
+}
+ 
+// student class
+class Student : Person
+{
+    // declare variables
+    public int StudentID { get; set; }
+    public int StudentAverage { get; set; }
+
+    // default constructor
+    public Student () : base()
+    {
+        StudentID = 0;
+        StudentAverage = 0;
+    }
+
+    // user constructor
+    public Student (int studentID, int studentAverage, string emailAddress, string firstName, string lastName, int phoneNumber) : base (emailAddress, firstName, lastName, phoneNumber)
+    {
+        StudentID = studentID;
+        StudentAverage = studentAverage;
+    }
+
+    // base override
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Student ID: {StudentID}, Student Average: {StudentAverage}";
+    }
+}
+
+// teacher class
+class Teacher : Person
+{
+    // declare variables
+    public string TeacherClass { get; set; }
+    public int Salary { get; set; }
+
+    // deafault constructor
+    public Teacher () : base ()
+    {
+        TeacherClass = "";
+        Salary = 0;
+    }
+
+    // user constructor
+    public Teacher (string teacherClass, int salary, string emailAddress, string firstName, string lastName, int phoneNumber) : base (emailAddress, firstName, lastName, phoneNumber)
+    {
+        TeacherClass = teacherClass;
+        Salary = salary;
+    }
+
+    // base override
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Class: {TeacherClass}, Salary: {Salary}";
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World");
+        List<int> firstList = new List<int>();
+        Console.WriteLine(Convert.ToString(firstList));
     }
 }
